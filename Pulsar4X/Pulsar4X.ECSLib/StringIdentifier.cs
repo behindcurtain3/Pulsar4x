c3e809ff023d2c515e2a9f33f4917488f2a54925
Justin Brown <behindcurtain3@gmail.com>
2023-08-24 12:10:32 -0700

    Add StringIdentifier
    Provides a human readable identifier to replace the use of Guid's.

---
+++ Pulsar4X/Pulsar4X.ECSLib/StringIdentifier.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Provides a human readable identifier for data.
+///
+/// Because it is a record it is not mutable and provides equality
+/// and hash code functionality by default.
+/// Example: var id = new StringIdentifer("base.rp1-engine");
+/// Example: var id = new StringIdentifier("base.nestedScope.rp1-engine");
+/// </summary>
+/// <param name="Scopes"></param>
+/// <param name="Name"></param>
+public record StringIdentifier(List<string> Scopes, string Name)
+{
+    public StringIdentifier(string singleScope, string itemName) : this(new List<string> { singleScope }, itemName)
+    {
+        if (string.IsNullOrEmpty(singleScope) || string.IsNullOrEmpty(itemName))
+        {
+            throw new ArgumentException("Both scope and itemName must be provided.");
+        }
+    }
+
+    public StringIdentifier(string fullIdentifier) : this(
+        fullIdentifier.Split('.', StringSplitOptions.RemoveEmptyEntries)
+                      .TakeWhile((part, index) => index < fullIdentifier.Count(c => c == '.'))
+                      .ToList(),
+        fullIdentifier.Split('.').Last())
+    {
+        if (fullIdentifier.Count(c => c == '.') < 1)
+        {
+            throw new ArgumentException("Invalid identifier format. Must be in the form 'scope1.scope2...Name'.");
+        }
+    }
+
+    public override string ToString() => $"{string.Join('.', Scopes)}.{Name}";
+}
